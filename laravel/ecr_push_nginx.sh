c802b5c0050b08853946a7e40af3e761fe17a407
yoshio <dayecaojian@gmail.com>
2022-09-26 16:43:43 +0900

    fix laravel ecr_push.sh

--- laravel/ecr_push_nginx.sh
+++ laravel/ecr_push_nginx.sh
@@ -2,6 +2,6 @@
 
 GIT_COMMIT_ID=$(git log --format="%H" -n 1)
 aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 806467626197.dkr.ecr.ap-northeast-1.amazonaws.com
-docker build -t zenn-backend-nginx:"${GIT_COMMIT_ID}" .
+docker build -t zenn-backend-nginx:"${GIT_COMMIT_ID}" -f ./infra/web/Dockerfile .
 docker tag zenn-backend-nginx:"${GIT_COMMIT_ID}" 806467626197.dkr.ecr.ap-northeast-1.amazonaws.com/zenn-nginx:"${GIT_COMMIT_ID}"
 docker push 806467626197.dkr.ecr.ap-northeast-1.amazonaws.com/zenn-nginx:"${GIT_COMMIT_ID}"
--- laravel/ecr_push_php_fpm.sh
+++ laravel/ecr_push_php_fpm.sh
@@ -2,6 +2,6 @@
 
 GIT_COMMIT_ID=$(git log --format="%H" -n 1)
 aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 806467626197.dkr.ecr.ap-northeast-1.amazonaws.com
-docker build -t zenn-backend-phpfpm:"${GIT_COMMIT_ID}" .
+docker build -t zenn-backend-phpfpm:"${GIT_COMMIT_ID}" -f ./infra/php/Dockerfile .
 docker tag zenn-backend-phpfpm:"${GIT_COMMIT_ID}" 806467626197.dkr.ecr.ap-northeast-1.amazonaws.com/zenn-php-fpm:"${GIT_COMMIT_ID}"
 docker push 806467626197.dkr.ecr.ap-northeast-1.amazonaws.com/zenn-php-fpm:"${GIT_COMMIT_ID}"
