4f098a4d95b1abb502155f680ffac3a2654562e1
yoshio <dayecaojian@gmail.com>
2022-09-27 10:59:22 +0900

    fix buildspec and next env

---
+++ laravel/deploy/seeder/specific_seeding.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+seeders=()
+if [ ${#seeders[@]} -gt 0 ]; then
+  # 拡張子がphpのファイルのPathが自動的に追記される
+  composer dump-autoload
+  for seeder_class in ${seeders[@]}; do
+   php artisan db:seed --class=$seeder_class
+  done
+fi
